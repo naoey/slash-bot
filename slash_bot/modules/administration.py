8ae7986f91e771bb320d4bcdbafa483f7203d20f
naoey <yog.venkatesh@gmail.com>
2016-10-20 13:59:53 +0530

    Fixed missing super() call for resolving permissions, tweaked some stuff~
    - Subsequent invocations of slow by admins will not have those messages deleted
    - Ignore per user slows when whole channel is already slowed with proper message

--- slash_bot/modules/administration.py
+++ slash_bot/modules/administration.py
@@ -8,7 +8,7 @@ Created on 2016-08-23
 import logging
 import datetime
 
-from discord import Forbidden
+from discord import Forbidden, NotFound
 
 import config
 
@@ -30,12 +30,13 @@ class SlowMode(Command):
     required_permissions = [Permissions.SERVER_ADMIN, ]
 
     class Slower(object):
-        def __init__(self, channel=None, interval=5, user=None):
+        def __init__(self, channel=None, interval=5, user=None, slowed_by=None):
             self.interval = int(interval)
             self.channel = channel
             self.user = user["id"] if user is not None else None
             self.uname = user["name"] if user is not None else None
             self.slowid = channel.id + "_" + user["id"] if user is not None else None
+            self.slowed_by = slowed_by
 
             self._subscription = BOT.subscribe_channel_messages(channel.id, self.clean)
 
@@ -50,6 +51,12 @@ class SlowMode(Command):
         async def clean(self, message):
             if not self.enabled:
                 return
+
+            if message.content.startswith(config.BOT_PREFIX):
+                first_word = message.content[1:].split(" ")[0]
+                if (Permissions.can(message.channel, message.author, permission=Permissions.SERVER_ADMIN) and
+                        (first_word == SlowMode.command or first_word in SlowMode.aliases)):
+                    return
 
             if self.last_message_time is None:
                 if self.user is not None:
@@ -71,6 +78,8 @@ class SlowMode(Command):
                             self.channel
                         )
                         await self.end()
+                    except NotFound:
+                        pass
 
             self.last_message_time = message.timestamp
 
@@ -84,6 +93,7 @@ class SlowMode(Command):
 
     @overrides(Command)
     async def make_response(self):
+        await super().make_response()
         if not self.source_channel.server.get_member(BOT.user.id).permissions_in(self.source_channel).manage_messages:
             self.response = "{} Gib me permission to manage messages here!".format(self.invoker.mention)
             return
@@ -100,7 +110,7 @@ class SlowMode(Command):
                 for each in existing_user_slows:
                     await each.end()
 
-                SlowMode.Slower(self.source_channel)
+                SlowMode.Slower(self.source_channel, slowed_by=self.invoker.id)
                 self.response = "Slow mode enabled, shush"
 
         elif len(self.params) == 1:
@@ -109,10 +119,14 @@ class SlowMode(Command):
                     await _alive_listeners[self.source_channel.id].end()
                     self.response = "Slow mode disabled, spemmmm!"
                 else:
-                    SlowMode.Slower(self.source_channel, self.params[0])
+                    SlowMode.Slower(self.source_channel, self.params[0], slowed_by=self.invoker.id)
                     self.response = "Slow mode enabled, spam limited to once every {} second(s)".format(self.params[0])
 
             elif self.params[0].startswith("<@"):
+                if self.source_channel.id in _alive_listeners.keys():
+                    self.response = "The whole channel is slowed already! Disable it to slow per-user."
+                    return
+
                 uid = self.params[0][2:-1]
                 user = next((x for x in self._raw_message.mentions if x.id == uid))
                 slowid = self.source_channel.id + "_" + uid
@@ -126,11 +140,16 @@ class SlowMode(Command):
                         user={
                             "id": uid,
                             "name": user.name,
-                        }
+                        },
+                        slowed_by=self.invoker.id,
                     )
                     self.response = "{} is now being spam-limited".format(user.name)
 
         elif len(self.params) == 2 and self.params[0].startswith("<@") and self.params[1].isdigit():
+            if self.source_channel.id in _alive_listeners.keys():
+                self.response = "{} the whole channel is slowed already! Disable it to slow per-user.".format(self.invoker.mention)
+                return
+
             uid = self.params[0][2:-1]
             user = next((x for x in self._raw_message.mentions if x.id == uid))
             slowid = self.source_channel.id + "_" + uid
@@ -145,7 +164,8 @@ class SlowMode(Command):
                     user={
                         "id": uid,
                         "name": user.name,
-                    }
+                    },
+                    slowed_by=self.invoker.id,
                 )
                 self.response = "{} is now being spam-limited to 1 message per {} second(s)".format(user.name, self.params[1])
 
